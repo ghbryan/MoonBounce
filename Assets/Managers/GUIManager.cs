7ecfe8f494dc7957acaed237b5f35f8040baa12d
Bryan Farmer <ghbryan@gmail.com>
2012-09-12 18:45:37 -0700

    Added Debug UI management. Fixed editor tabbing.

--- Assets/Managers/GUIManager.cs
+++ Assets/Managers/GUIManager.cs
@@ -3,15 +3,23 @@ using System.Collections;
 
 public class GUIManager : MonoBehaviour {
 	
-	public GUIText velocityText;
-	public bool debugUI = false;
+	public GUIText velocityText, gameTitle;
+	public bool debugMode = false;
 	
 	private static GUIManager instance;
 	
 	// Use this for initialization
 	void Start () {
 		instance = this;
-		velocityText.enabled = debugUI;
+		
+		gameTitle.enabled = false; //Main Menu: Title
+		
+		if(debugMode) {
+			velocityText.enabled = true;
+		} 
+		else {
+			velocityText.enabled = false;
+		}
 	}
 	
 	// Update is called once per frame
